2f37a104380a9b7a82cb82936b044b2aa8e36780
Oleh Fliurkevych <oleh0809@gmail.com>
2022-04-27 10:29:48 +0300

    Create Task1.java file

---
+++ task2/Task1.java
@@ -0,0 +1,14 @@
+/**
+ * @author Oleh Fliurkevych
+ */
+public class Task1 {
+
+  public static void main(String[] args) {
+    System.out.println(add(3, 4));
+  }
+  
+  public static int add(int a, int b){
+    return a + b;
+  }
+
+}
