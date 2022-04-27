2d75e9a0263c142c8f32b00dc256db399edd18a2
Oleh Fliurkevych <oleh0809@gmail.com>
2022-04-27 11:35:32 +0300

    Add add operation for 3 params

--- task2/Task1.java
+++ task2/Task1.java
@@ -6,10 +6,15 @@ public class Task1 {
   public static void main(String[] args) {
     System.out.println(add(3, 4));
   }
-  
-  public static int add(int a, int b){
-    System.out.println("This add method");
+
+  public static int add(int a, int b) {
+    System.out.println("This add method for 2 params");
     return a + b;
+  }
+
+  public static int add(int a, int b, int c) {
+    System.out.println("This add method for 3 params");
+    return a + b + c;
   }
 
 }
